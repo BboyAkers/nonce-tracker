2640b2b2e1121b2c472450a849220da4dec60127
unknown <austin.akers5@gmail.com>
2020-10-31 00:15:06 -0700

    refactor start

--- index.ts
+++ index.ts
@@ -1,30 +1,31 @@
 const EthQuery = require('ethjs-query')
 const assert = require('assert')
 const Mutex = require('await-semaphore').Mutex
-/**
-  @param opts {Object}
-    @param {Object} opts.provider a ethereum provider
-    @param {Function} opts.getPendingTransactions a function that returns an array of txMeta
-    whosee status is `submitted`
-    @param {Function} opts.getConfirmedTransactions a function that returns an array of txMeta
-    whose status is `confirmed`
-  @class
-*/
-
-class NonceTracker {
-
-  constructor ({ provider, blockTracker, getPendingTransactions, getConfirmedTransactions }) {
-    this.provider = provider
-    this.blockTracker = blockTracker
-    this.ethQuery = new EthQuery(provider)
-    this.getPendingTransactions = getPendingTransactions
-    this.getConfirmedTransactions = getConfirmedTransactions
+// /**
+//   @param opts {Object}
+//     @param {Object} opts.provider a ethereum provider
+//     @param {Function} opts.getPendingTransactions a function that returns an array of txMeta
+//     whosee status is `submitted`
+//     @param {Function} opts.getConfirmedTransactions a function that returns an array of txMeta
+//     whose status is `confirmed`
+//   @class
+// */
+interface ObjectProps {
+    provider: string, 
+    blockTracker: string, 
+    getPendingTransactions(string): string[], 
+    getConfirmedTransactions(): boolean[]
+}
+export class NonceTracker {
+  // { provider<string>, blockTracker<string>, getPendingTransactions<function>, getConfirmedTransactions<function> }
+  constructor (public properties: ObjectProps) {
+    this.ethQuery = new EthQuery(properties.provider)
     this.lockMap = {}
   }
 
-  /**
-    @returns {Promise<Object>} with the key releaseLock (the gloabl mutex)
-  */
+  // /**
+  //   @returns {Promise<Object>} with the key releaseLock (the gloabl mutex)
+  // */
   async getGlobalLock () {
     const globalMutex = this._lookupMutex('global')
     // await global mutex free
@@ -32,20 +33,20 @@ class NonceTracker {
     return { releaseLock }
   }
 
-  /**
-   * @typedef NonceDetails
-   * @property {number} highestLocallyConfirmed - A hex string of the highest nonce on a confirmed transaction.
-   * @property {number} nextNetworkNonce - The next nonce suggested by the eth_getTransactionCount method.
-   * @property {number} highestSuggested - The maximum between the other two, the number returned.
-   */
+  // /**
+  //  * //@typedef NonceDetails
+  //  * @property {number} highestLocallyConfirmed - A hex string of the highest nonce on a confirmed transaction.
+  //  * @property {number} nextNetworkNonce - The next nonce suggested by the eth_getTransactionCount method.
+  //  * @property {number} highestSuggested - The maximum between the other two, the number returned.
+  //  */
 
-  /**
-  this will return an object with the `nextNonce` `nonceDetails`, and the releaseLock
-  Note: releaseLock must be called after adding a signed tx to pending transactions (or discarding).
+  // /**
+  // this will return an object with the `nextNonce` `nonceDetails`, and the releaseLock
+  // Note: releaseLock must be called after adding a signed tx to pending transactions (or discarding).
 
-  @param address {string} the hex string for the address whose nonce we are calculating
-  @returns {Promise<NonceDetails>}
-  */
+  // @param address {string} the hex string for the address whose nonce we are calculating
+  // @returns {Promise<NonceDetails>}
+  // */
   async getNonceLock (address) {
     // await global mutex free
     await this._globalMutexFree()
@@ -82,13 +83,13 @@ class NonceTracker {
     }
   }
 
-  async _globalMutexFree (): Promise<void> {
+  async _globalMutexFree () {
     const globalMutex = this._lookupMutex('global')
     const releaseLock = await globalMutex.acquire()
     releaseLock()
   }
 
-  async _takeMutex (lockId): Promise<any> {
+  async _takeMutex (lockId) {
     const mutex = this._lookupMutex(lockId)
     const releaseLock = await mutex.acquire()
     return releaseLock
@@ -131,17 +132,17 @@ class NonceTracker {
     return highestNonce
   }
 
-  /**
-    @typedef {object} highestContinuousFrom
-    @property {string} - name the name for how the nonce was calculated based on the data used
-    @property {number} - nonce the next suggested nonce
-    @property {object} - details the provided starting nonce that was used (for debugging)
-  */
-  /**
-    @param txList {array} - list of txMeta's
-    @param startPoint {number} - the highest known locally confirmed nonce
-    @returns {highestContinuousFrom}
-  */
+  // /**
+  //   @typedef {object} highestContinuousFrom
+  //   @property {string} - name the name for how the nonce was calculated based on the data used
+  //   @property {number} - nonce the next suggested nonce
+  //   @property {object} - details the provided starting nonce that was used (for debugging)
+  // */
+  // /**
+  //   @param txList {array} - list of txMeta's
+  //   @param startPoint {number} - the highest known locally confirmed nonce
+  //   @returns {highestContinuousFrom}
+  // */
   _getHighestContinuousFrom (txList, startPoint) {
     const nonces = txList.map((txMeta) => {
       const nonce = txMeta.txParams.nonce
@@ -159,4 +160,4 @@ class NonceTracker {
 
 }
 
-module.exports = NonceTracker
+// module.exports = NonceTracker
